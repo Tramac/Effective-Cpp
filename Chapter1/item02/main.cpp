1e60adfc22251e4d99c967d0af43700dca6f934a
Tramac <1145955920@qq.com>
2019-01-06 06:22:38 -0600

    Create main.cpp

---
+++ Chapter1/item02/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+#define ASPECT_RATIO 1.653
+const double AspectRatio = 1.653;
+int main()
+{
+	cout << "ASPECT_RATIO: " << ASPECT_RATIO << endl;
+	cout << "AspectRatio: " << AspectRatio << endl;
+	return 0;
+}
