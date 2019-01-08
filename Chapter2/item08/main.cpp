af9d249480003ef366ca4f4634217fe3f816566d
Tramac <1145955920@qq.com>
2019-01-08 03:21:13 -0600

    Update main.cpp

--- Chapter2/item08/main.cpp
+++ Chapter2/item08/main.cpp
@@ -6,14 +6,14 @@ int main()
     // -----Demo 2.-----
     DBConn dbc(DBConnection::create()); // 建立DBConnection对象并交给DBConn的接口
 
-	  // -----Demo 3.-----
-	  DBConn2 dbc2(DBConnection::create());
-	  try {
-		  dbc2.close();
-	  }
-	  catch(...) {
+    // -----Demo 3.-----
+    DBConn2 dbc2(DBConnection::create());
+    try {
+	  dbc2.close();
+	}
+    catch(...) {
 
-	  }
+    }
 
-	  return 0;
+    return 0;
 }
