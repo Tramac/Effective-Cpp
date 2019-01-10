570e6048bfe053d42720504ecc7accf7ce8eb228
Tramac <1145955920@qq.com>
2019-01-10 08:37:27 -0600

    Create main.cpp

---
+++ Chapter4/item20/main.cpp
@@ -0,0 +1,25 @@
+#include "head.h"
+
+// -----Demo 1.pass-by-value-----
+bool validateStudent(Student s) { // 函数以by value方式接受学生
+    return false;
+}
+
+// -----Demo 2.pass by reference-to-const-----
+bool validateStudent2(const Student& s) {
+    return true;
+}
+
+// -----Demo 3.-----
+void printNameAndDisplay(const Window& w) {
+	std::cout << w.name();
+	w.display();
+}
+
+int main()
+{
+	// -----Demo 1.-----
+	Student plato;
+	bool platoIsOk = validateStudent(plato); // 调用函数
+	return 0;
+}
