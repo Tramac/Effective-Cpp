6b88a8e413568e46f350370b05fbe4a08f8bd6ae
Tramac <1145955920@qq.com>
2019-01-10 05:36:52 -0600

    Update head.h

--- Chapter4/item18/head.h
+++ Chapter4/item18/head.h
@@ -16,16 +16,16 @@ class Month {
 public:
   static Month Jan() { return Month(1); }
   static Month Feb() { return Month(2); }
-	static Month Mar() { return Month(3); }
-	static Month apr() { return Month(4); }
-	static Month May() { return Month(5); }
-	static Month Jun() { return Month(6); }
-	static Month Jul() { return Month(7); }
-	static Month Aug() { return Month(8); }
-	static Month Sep() { return Month(9); }
-	static Month Oct() { return Month(10); }
-	static Month Nov() { return Month(11); }
-	static Month Dec() { return Month(12); }
+  static Month Mar() { return Month(3); }
+  static Month apr() { return Month(4); }
+  static Month May() { return Month(5); }
+  static Month Jun() { return Month(6); }
+  static Month Jul() { return Month(7); }
+  static Month Aug() { return Month(8); }
+  static Month Sep() { return Month(9); }
+  static Month Oct() { return Month(10); }
+  static Month Nov() { return Month(11); }
+  static Month Dec() { return Month(12); }
 
   Month() {}
 
