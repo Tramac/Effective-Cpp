fba9be91de490b5cf0844743a9fe5896a037c635
Tramac <1145955920@qq.com>
2019-01-10 05:37:25 -0600

    Create main.cpp

---
+++ Chapter4/item18/main.cpp
@@ -0,0 +1,13 @@
+#include "head.h"
+
+int main()
+{
+    // -----Demo 1.-----
+    // Date d(30, 3, 1995); // 错误！不正确的类型
+    // Date d(Day(30), Month(3), Year(1995)); // 错误！不正确的类型
+    // Date d(Month(3), Day(30), Year(1995)); // OK，类型正确
+
+    // -----Demo 2.-----
+
+    return 0;
+}
