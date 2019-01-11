2d8a6b3cacc5f4736fe778a5c41bab3633222fee
Tramac <1145955920@qq.com>
2019-01-10 20:44:16 -0600

    Create main.cpp

---
+++ Chapter4/item21/main.cpp
@@ -0,0 +1,18 @@
+#include "head.h"
+
+int main()
+{
+    // -----Demo 1.-----
+    Rational a(1, 2);
+    Rational b(3, 5);
+
+    // Rational c = a * b;
+    
+    Rational w, x, y, z;
+    w = x * y * z;
+
+    if ((x * y) == (z * w)) {
+    }
+
+    return 0;
+}
