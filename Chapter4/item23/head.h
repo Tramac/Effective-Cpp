cd4f7fc2cccb901459982cd41f96a0c2133acaaa
Tramac <1145955920@qq.com>
2019-01-10 21:58:35 -0600

    Create head.h

---
+++ Chapter4/item23/head.h
@@ -0,0 +1,16 @@
+// -----Demo 1.-----
+namespace WebBrowserStuff {
+  class WebBrowser {
+  public:
+    void clearCache();
+    void clearHistory();
+    void removeCookies();
+
+    // -----Method 1.member函数-----
+    // void clearEverathing() {
+    //   clearCache();
+    //   clearHistory();
+    //   removeCookies();
+    // };
+};
+}
